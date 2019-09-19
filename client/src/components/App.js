3f32dfb80f59a579e6442156fa34f3f04439a79b
Piotr Kielek <piotrek4kontak@gmail.com>
2019-09-19 12:33:21 +0200

    installing and testing react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>Page One</div>;
+};
+
+const PageTwo = () => {
+  return <div>Page Two</div>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
