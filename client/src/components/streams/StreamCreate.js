fe1cbb54793d72de8c59eb21cff7be412ff3b11e
Piotr Kielek <piotrek4kontak@gmail.com>
2019-09-20 19:33:19 +0200

    Added CRUD operations to streams

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import { Field, reduxForm } from "redux-form";
 import React from "react";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -19,9 +21,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit({ title, description }) {
-    console.log(title, description);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
   render() {
     return (
       <form
@@ -51,7 +53,12 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWrap = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrap);
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,70 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
+import { Link } from "react-router-dom";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderAdmin(userId) {
+    if (this.props.currendUserId === userId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">EDIT</button>
+          <button className="ui button negative">DELETE</button>
+        </div>
+      );
+    }
+  }
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: "right" }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
+  renderStreamList() {
+    return this.props.streams.map(({ title, description, id, userId }) => {
+      return (
+        <div className="item" key={id}>
+          {this.renderAdmin(userId)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            <div className="header">{title}</div>
+            <div className="description">{description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderStreamList()}</div>
+        {this.renderCreate()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams),
+    currendUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
-
-export default StreamList;
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
